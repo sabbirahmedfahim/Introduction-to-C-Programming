142c248fde3c3d3b9375973ec7bb3ac00ea8eccd
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2024-03-31 06:02:08 +0600

    module 1 done

---
+++ module_1.c
@@ -0,0 +1,16 @@
+/*
+stdio => standard input and output
+to print '\'=> printf("\\"); 
+int rahim; <=declare
+int rahim = 100; <= declare + assign
+scanf("%d", &n); <= '&' means address of n
+%c includes all characters, like @$%, not just alphabets
+TASK: print 10% 20%
+1 bit == 0 or 1
+1 byte == 8 bit 
+16 8 4 2
+bit to byte == 2^n -1
+int minimum value 10^9 == 1000000000
+long long int minimum value 10^18
+variable name can be start with _ (underscore) like _sabbir
+*/
