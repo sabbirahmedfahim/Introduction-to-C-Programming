079bf910f3bbcb4b2e47341464e6eaec5fb7d5c7
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2024-04-19 18:19:21 +0600

    silly mistake from assingnment02 is fixed & submitted

--- WEEK_02/assignment_02/is_a_challenge.c
+++ WEEK_02/assignment_02/is_a_challenge.c
@@ -3,24 +3,41 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    if (n>0)
+    if (n > 0)
     {
         for (int i = 1; i <= n; i++)
         {
             printf("%d ", i);
         }
-        
     }
-    else if (n<0)
+    else
     {
-        for (int i = n; i <=0; i++)
+        for (int i = n; i <= 0; i++)
         {
             printf("%d ", i);
         }
-        
     }
-    
-    
 
-   return 0;
+    return 0;
+}#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n > 0)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            printf("%d ", i);
+        }
+    }
+    else
+    {
+        for (int i = n; i <= 0; i++)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
 }
