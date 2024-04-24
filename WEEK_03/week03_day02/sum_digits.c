12d594f50aa0f7a4cc2f66c0fc921d3c5c73f0ef
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2024-04-25 02:42:57 +0600

    resolved one last problem from week03 day02 and fully finished the module

--- WEEK_03/week03_day02/sum_digits.c
+++ WEEK_03/week03_day02/sum_digits.c
@@ -3,7 +3,7 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    char arr[100001];
+    char arr[1000001]; //highest size of char in c is: 10^6 ***v.v.i
     scanf("%s", &arr);
     int sum = 0;
     for (int i = 0; i < n; i++)
